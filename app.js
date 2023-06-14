f56988ca13daed2c63e4b73ff746c51e8e3aaa8c
Desmond <asaredesmond634@gmail.com>
2023-06-14 19:44:51 +0000

    master

---
+++ app.js
@@ -0,0 +1,12 @@
+const btnEl = document.getElementById("btn");
+const billEl = document.getElementById("bill");
+const tipEL = document.getElementById("tip");
+const totalEl = document.getElementById("total");
+
+
+
+
+
+
+
+btnEl.addEventListener("click", calculateTip)
