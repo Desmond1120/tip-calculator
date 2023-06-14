bd60dbe0ef0f2ed509fca3f672c95087b8f92b9d
Desmond <asaredesmond634@gmail.com>
2023-06-14 23:40:14 +0000

    added functionality

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const btnEl = document.getElementById("btn");
-const billEl = document.getElementById("bill");
-const tipEL = document.getElementById("tip");
-const totalEl = document.getElementById("total");
+const btnEl = document.getElementById('calculate');
+const billInput = document.getElementById('bill');
+const tipInput = document.getElementById('tip');
+const totalInput = document.getElementById('total');
 
 
 
+const calculateTtotal = function(){
+ const billValue = billInput.value;
+ const tipValue = tipInput.value;
+ const totalValue = billValue * (1 + tipValue / 100)
+ totalInput.innerText = totalValue;
+}
 
-
-
-
-btnEl.addEventListener("click", calculateTip)
+btnEl.addEventListener("click", calculateTtotal);
